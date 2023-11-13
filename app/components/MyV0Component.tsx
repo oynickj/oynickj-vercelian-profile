da37807c30a6baf6049429226a379e7410513374
Josh Oynick <josh.oynick@vercel.com>
2023-11-13 11:08:30 -0500

    Update MyV0Component.tsx

--- app/components/MyV0Component.tsx
+++ app/components/MyV0Component.tsx
@@ -1,17 +1,10 @@
 /**
  * v0 by Vercel.
- * @see https://v0.dev/t/EK8CUYHCrpy
+ * @see https://v0.dev/t/uEmOzEC1lPi
  */
-import { Button } from "@/components/ui/button"
-
 export default function Component() {
   return (
-    <section className="w-full flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-gray-800">
-      <h1 className="text-3xl font-bold mb-4 dark:text-white">v0 Motivational Quotes</h1>
-      <Button className="mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Get Quote</Button>
-      <div className="p-4 max-w-xl mx-auto bg-white rounded-xl shadow-md space-x-2 text-center dark:bg-gray-900">
-        <p className="text-gray-500 dark:text-gray-300">Click the button to display a motivational quote.</p>
-      </div>
-    </section>
+    <div className="animate-bounce w-24 h-24 mx-auto rounded-full bg-gradient-to-r from-green-400 to-blue-500 dark:from-purple-400 dark:to-red-500" />
   )
 }
+
